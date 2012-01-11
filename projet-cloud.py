299af5a61e51e707b0928ed5a6a4698992afbb8a
Bruno Jurkovski <bruno91j@gmail.com>
2012-01-11 12:14:05 +0100

    added a template file

--- projet-cloud.py
+++ projet-cloud.py
@@ -1,8 +1,16 @@
 import webapp2
+import jinja2
+import os
+
+PAGES_FOLDER = "pages/"
+jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainPage(webapp2.RequestHandler):
 	def get(self):
-		self.response.headers['Content-Type'] = 'text/plain'
-		self.response.out.write('Hello, webapp World!')
+		template_values = {
+			'param': 'This is a parameter to the page renderer!'
+		}
+		template = jinja.get_template(PAGES_FOLDER + 'index.html')
+		self.response.out.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
